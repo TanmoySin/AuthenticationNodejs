9ed9b680438ec16e326799cc45dbca0badc708d6
Tanmoy Sinha <tanmoysin1997@gmail.com>
2023-07-21 22:43:22 +0530

    AuthenticationNodejs

---
+++ Readme.txt
@@ -0,0 +1,5 @@
+Name of the project is Node Js Authentication Page.
+
+Download the all the files and install all Dependencies using the "npm install", after then Run the Project with the command "npm start".
+
+now open the browser and run the program in LocalHost:4000
---
+++ config/key.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: "mongodb://tanmoysin1997:Iamgood111@ac-o1afx2o-shard-00-00.2swpsxg.mongodb.net:27017,ac-o1afx2o-shard-00-01.2swpsxg.mongodb.net:27017,ac-o1afx2o-shard-00-02.2swpsxg.mongodb.net:27017/?ssl=true&replicaSet=atlas-8mmslk-shard-0&authSource=admin&retryWrites=true&w=majority"
+}
